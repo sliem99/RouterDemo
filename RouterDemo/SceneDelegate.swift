44735ac026c4f0f860022429e779f4fde86214e1
Mohamed Sliem <m.sliem@solutionplus.net>
2025-01-16 10:22:59 +0200

    SceneDelegate: Fix window unwrapping

--- RouterDemo/SceneDelegate.swift
+++ RouterDemo/SceneDelegate.swift
@@ -13,8 +13,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let scene = (scene as? UIWindowScene), var window else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
+        guard let window else { return }
         let app = App(window: window)
         app.start()
     }
